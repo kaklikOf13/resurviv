c8d1e69ddf0e24b580f4d450a11c33b4581ede98
kaklikOf13 <canaldokaklik@gmail.com>
2024-06-28 18:19:04 -0300

    feat:healing-after-kill-plugin

---
+++ server/src/plugins/healingAfterKill.ts
@@ -0,0 +1,16 @@
+import { ObjectType } from "../../../shared/utils/objectSerializeFns"
+import { EventType, GamePlugin } from "../utils/plugins"
+export class HealingAfterKillPlugin extends GamePlugin{
+    healingAmount:number
+    constructor(healingAmount:number){
+        super()
+        this.healingAmount=healingAmount
+    }
+    initSignals(): void {
+        this.on(EventType.PlayerDie,(e)=>{
+            if(e.killer.source&&e.killer.source.__type==ObjectType.Player){
+                e.killer.source.health+=this.healingAmount
+            }
+        })
+    }
+}
